c9a30ac1b8a451e55b9902b073e5b9c3dc1ef83b
Thang <thangchien86@gmail.com>
2023-03-01 13:54:00 +0700

    Frist

--- configs/DBConnection.php
+++ configs/DBConnection.php
@@ -6,7 +6,7 @@ class DBConnection{
     public function __construct(){
          // B1. Kết nối DB Server
          try {
-            $this->conn = new PDO('mysql:host=localhost;dbname=demo_;port=3306', 'root','');
+            $this->conn = new PDO('mysql:host=localhost;dbname=btth01_cse485;port=3306', 'root','');
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
