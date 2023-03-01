e9e070e766c91588a971370f179f89d792f686b7
Nguyen Manh Tuan <obmhenry12345@gmail.com>
2023-03-01 14:40:30 +0700

    header footer

--- services/ArticleService.php
+++ services/ArticleService.php
@@ -8,15 +8,11 @@ class ArticleService{
        $conn = $dbConn->getConnection();
 
         // B2. Truy vấn
-        $sql = "SELECT * FROM article INNER JOIN category ON article.category_id=category.id";
-        $stmt = $conn->query($sql);
+        $sql = "SELECT * FROM article INNER JOIN category ON article.category_id=category.id"; // INNER JOIN category ON article.category_id=category.id
+
 
         // B3. Xử lý kết quả
         $articles = [];
-        while($row = $stmt->fetch()){
-            $article = new Article($row['title'], $row['summary'], $row['name']);
-            array_push($articles,$article);
-        }
         // Mảng (danh sách) các đối tượng Article Model
 
         return $articles;
