79ba8e8bd2992b519276ddd456d9a5a656937fef
Nguyen Manh Tuan <obmhenry12345@gmail.com>
2023-03-01 20:42:47 +0700

    article service

--- views/home/index.php
+++ views/home/index.php
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 <?php 
-    require_once('./CSE485_2023_BTTH02/configs/DBConnection.php');
-    include 'header.php'; 
+    include 'header.php';
+    include '/htdocs/CSE485_2023_BTTH02/configs/DBConnection.php';
 ?>
 
         <div id="carouselExampleIndicators" class="carousel slide">
@@ -35,19 +34,15 @@
     <main class="container-fluid mt-3" style="margin-bottom: 5%;">
         <h3 class="text-left text-uppercase mb-3 text-primary fw-bold fs-2 text-danger">TOP bài hát yêu thích</h3>
         <div class="row">
-            <?php
+        <?php
                 // Ket noi db
-                $conn = mysqli_connect('localhost','root','','btth01_cse485');
-                if(!$conn){
-                    die('Kết nối tới Server lỗi');
-                }
+                $dbConn = new DBConnection();
+                $conn = $dbConn->getConnection();
                 // Truy van
                 $sql = "SELECT * FROM baiviet ORDER BY ma_bviet";
-                $result = mysqli_query($conn, $sql); 
+                $result = $conn->query($sql);
                 // Xu li ket qua tra ve
-                if(mysqli_num_rows($result) > 0){
-                    while($row = mysqli_fetch_assoc($result)){
-                       
+
             ?>
                 <div class="col-sm-3">
                     <div class="card mb-2 bg-danger border-red" style="width: 100%;">
@@ -61,28 +56,8 @@
                         </div>
                     </div>
                 </div>
-=======
->>>>>>> 689d25e184d84900bf0e246cce9a8f977381661c
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <ul>
-        <li><a href="./index.php">Trang chủ</a></li>
-        <li><a href="./index.php?controller=catelogy&action=list">Thể loại</a></li>
-    </ul>
-    <h1>Tôi là TRANG CHỦ</h1>
-    <?php
-        foreach($categories as $category){
-            echo "<br>{$category->getTitle()}"."        "."{$category->getTen()}</br>";
-        }
-    ?>
-</body>
-</html>
-
+            <?php
+            ?>
+    </main>
+<?php include 'footer.php';?>
